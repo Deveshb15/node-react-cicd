6a36275ffb9b93d0fea7baec3eeede9c7d35e9f1
Deveshb15 <deveshb583@gmail.com>
2022-05-14 20:30:16 +0530

    fix: dist instead of build for vite-react

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/dist')))
 
 app.get('/name', (req, res) => {
     res.json({
